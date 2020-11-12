aa130bf31247d82ab4db7b6305ab99959cda440c
kunmingqaq@163.com <kunmingqaq@163.com>
2020-11-12 16:25:32 +0800

    新上传

--- src/test/java/com/example/service/PersonService.java
+++ src/test/java/com/example/service/PersonService.java
@@ -1,9 +1,12 @@
 package com.example.service;
 
 import com.example.entity.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonService {
-    
+    @GetMapping("/hello")
     public Person getPerson() {
         return new Person ();
     }
